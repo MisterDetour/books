9e1257ef959e738df372a0b3b944ea092dff70f3
Detour <detour@hybridvigordesign.com>
2022-08-08 14:42:49 -0700

    Quick loader test

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -25,8 +25,8 @@ const HomePage = () => {
       <h1>Reading List</h1>
       <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <CategoryNavCell userId={currentUser.id} />
-      {/* <BookshelfCell userId={currentUser.id} />
-      {newBookForm && <NewBookCell userId={currentUser.id} />}
+      <BookshelfCell userId={currentUser.id} />
+      {/* {newBookForm && <NewBookCell userId={currentUser.id} />}
       <AdminNav /> */}
     </>
   )
